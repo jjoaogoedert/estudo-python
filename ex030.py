0e0a58c5afe329fa4f35c95bdc21a0c6f526612c
Joao Goedert <joaogueda@hotmail.com>
2025-03-16 11:35:45 -0300

    Estudo de python com curso

---
+++ ex030.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero: "))
+
+if numero % 2 == 0:
+    print("O numero que você digitou, {} , é par".format(numero))
+else:
+    print("O numero que você digitou {} , é inpar".format(numero))
